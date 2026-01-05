05be2ee8cbb4b375ec30e87c3258eabf6006fdbd
ES_PREPA_DEV <setifeliott@gmail.com>
2026-01-05 16:59:58 +0100

    Init project

--- app/src/index.js
+++ app/src/index.js
@@ -2,3 +2,14 @@
 import './assets/css/style.css';
 
 console.log( 'Allez, au boulot ! 🚀' );
+
+const elH1 = document.createElement('h1');
+elH1.textContent = 'Salut';
+
+document.body.append( elH1 );
+
+const elCanvas = document.createElement('canvas');
+elCanvas.width = 800;
+elCanvas.height = 600;
+
+document.body.append( elCanvas );
