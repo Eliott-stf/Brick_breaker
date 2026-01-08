bd7d6c370cd86a0217e47bd4179564df87959963
ES_PREPA_DEV <setifeliott@gmail.com>
2026-01-08 16:53:31 +0100

    Edge draw

--- app/src/Game/Game.js
+++ app/src/Game/Game.js
@@ -7,9 +7,10 @@ import brickImgSrc from '../assets/img/brick.png';
 import CustomMath from './CustomMath';
 import Ball from './Ball';
 import Vector from './DataType/Vector';
+import edgeImgSrc from '../assets/img/edge.png'
+import GameObject from './GameObject';
 
-class Game
-{
+class Game {
     // Contexte de dessin du canvas
     ctx;
 
@@ -17,13 +18,18 @@ class Game
     images = {
         ball: null,
         paddle: null,
-        brick: null
+        brick: null,
+        edge: null
     };
 
     // State (un objet qui décrit l'état actuel du jeu, les balles, les briques encore présentes, etc.)
     state = {
         // Balles (plusieurs car possible multiball)
         balls: [],
+        //Bordure de la mort
+        deathEdge: null,
+        //Bordure à rebond
+        bouncingEdges: [],
         // Paddle
         paddle: null
     };
@@ -37,7 +43,7 @@ class Game
         // Initialisation des objets du jeu
         this.initGameObjects();
         // Lancement de la boucle
-        requestAnimationFrame( this.loop.bind(this) );
+        requestAnimationFrame(this.loop.bind(this));
         // Après la boucle
     }
 
@@ -46,11 +52,11 @@ class Game
         const elH1 = document.createElement('h1');
         elH1.textContent = 'Arkanoïd';
 
-        const elCanvas = document.createElement( 'canvas' );
+        const elCanvas = document.createElement('canvas');
         elCanvas.width = 800;
         elCanvas.height = 600;
-        
-        document.body.append( elH1, elCanvas );
+
+        document.body.append(elH1, elCanvas);
 
         // Récupération du contexte de dessin
         this.ctx = elCanvas.getContext('2d');
@@ -72,46 +78,77 @@ class Game
         const imgBrick = new Image();
         imgBrick.src = brickImgSrc;
         this.images.brick = imgBrick;
+
+        //Bord
+        const imgEdge = new Image();
+        imgEdge.src = edgeImgSrc;
+        this.images.edge = imgEdge;
     }
 
     // Mise en place des objets du jeu sur la scene
     initGameObjects() {
         // Balle
         const ball = new Ball(this.images.ball, 20, 20, 45, 4);
-        ball.setPosition( 400, 300 );
-        this.state.balls.push( ball );
+        ball.setPosition(400, 300);
+        this.state.balls.push(ball);
 
         // Dessin des balles
-        this.state.balls.forEach( theBall => {
+        this.state.balls.forEach(theBall => {
             theBall.draw();
+        });
+
+        //Bordure de la mort
+        const deathEdge = new GameObject(this.images.edge, 800, 20);
+        deathEdge.setPosition(0, 630);
+        this.state.deathEdge = deathEdge;
+
+        //On le dessine ou pas ? 
+
+        //Bordure a rebond
+        const edgeTop = new GameObject(this.images.edge, 800, 20);
+        edgeTop.setPosition(0, 0);
+        const edgeRight = new GameObject(this.images.edge, 20, 610);
+        edgeRight.setPosition(780, 20);
+        const edgeLeft = new GameObject(this.images.edge, 20, 610);
+        edgeLeft.setPosition(0, 20);
+        this.state.bouncingEdges.push(edgeTop, edgeRight, edgeLeft);
+
+        //Dessin des bordures
+        this.state.bouncingEdges.forEach(theEdge => {
+            theEdge.draw();
         });
     }
 
     // Boucle d'animation
     loop() {
         // On efface tous le canvas
-        this.ctx.clearRect( 0, 0, 800, 600 );
+        this.ctx.clearRect(0, 0, 800, 600);
+
+        //Dessin des bordures
+        this.state.bouncingEdges.forEach(theEdge => {
+            theEdge.draw();
+        });
 
         // Cycle des balles
-        this.state.balls.forEach( theBall => {
+        this.state.balls.forEach(theBall => {
             theBall.update();
 
+            //TODO:Remplacer par la collision avec les edges
             const bounds = theBall.getBounds();
             // TODO: en mieux : Détection des collisions
             // Collision avec le côté droite ou gauche de la scène : Inversion du X de la velocité
-            if( bounds.right >= 800 || bounds.left <= 0 ) {
+            if (bounds.right >= 800 || bounds.left <= 0) {
                 theBall.reverseOrientationX();
             }
             // Collision avec le côté bas ou haut de la scène : Inversion du Y de la velocité
-            if( bounds.bottom >= 600 || bounds.top <= 0 ) {
+            if (bounds.bottom >= 600 || bounds.top <= 0) {
                 theBall.reverseOrientationY();
             }
-
             theBall.draw();
         });
 
         // Appel de la frame suivante
-        requestAnimationFrame( this.loop.bind(this) );
+        requestAnimationFrame(this.loop.bind(this));
     }
 
     // Fonction de test inutile dans le jeu
