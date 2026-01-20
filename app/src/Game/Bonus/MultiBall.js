efa95e59859508570ad217ef1d7ddd9a865cd536
ES_PREPA_DEV <setifeliott@gmail.com>
2026-01-20 11:13:59 +0100

    New asset 8-bit, Multiball 2/2

--- app/src/Game/Bonus/MultiBall.js
+++ app/src/Game/Bonus/MultiBall.js
@@ -4,12 +4,16 @@ import Ball from '../MovingObjects/Ball';
 
 export default class MultiBall extends BonusBase {
     trigger() {
-        
+
         // On crée 5 nouvelles instances de balle 
         for (let i = 0; i < 5; i++) {
             const ballDiameter = theGame.config.ball.radius * 2;
-            const ball = new Ball(theGame.images.ball, ballDiameter, ballDiameter, theGame.config.ball.orientation, theGame.config.ball.speed);
-            ball.setPosition(theGame.state.balls.position.x + (i * 20), theGame.state.balls.position.y - (i * 30));
+
+            //On fait varié l'orientation 
+            const ball = new Ball(theGame.images.ball, ballDiameter, ballDiameter, theGame.config.ball.orientation + (i * 60), theGame.config.ball.speed);
+
+            //on positionne au meme endroit que la balle "principale"
+            ball.setPosition(theGame.state.balls[0].position.x, theGame.state.balls[0].position.y);
             ball.isCircular = true;
             theGame.state.balls.push(ball);
         }
